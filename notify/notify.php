3408a1fdbca174e869a34264914b9688d9ad8f33
JeremieColle <jeremie.colle@cinetpay.com>
2021-07-29 17:30:25 +0000

    up-29

--- notify/notify.php
+++ notify/notify.php
@@ -4,20 +4,22 @@ print_r($_POST);
 //check if there is a cinetpay post value
 if (isset($_POST['cpm_trans_id'])) {
     try {
-        echo "C'est ok";
-        // call required lib
-        require_once __DIR__ . '/../src/new-guichet.php';
-        // sample class for simulate payment validation
-        require_once __DIR__ . '/../commande.php';
-        $commande = new Commande();
+    // call required lib
+    require_once __DIR__ . '/../src/new-guichet.php';
+
+    // sample class for simulate payment validation
+    require_once __DIR__ . '/../commande.php';
+
+    $commande = new Commande();
         // cinetpay class initialisation and transaction identify
-        $id_transaction = htmlspecialchars($_POST['cpm_trans_id']);
+        $id_transaction = $_POST['cpm_trans_id'];
         // enter apiKey
         $apikey = "12912847765bc0db748fdd44.40081707";
         //enter siteId
         $site_id = "445160";
         //version 
         $version = "V2";
+
         $CinetPay = new CinetPay($site_id, $apikey, $version);
         // get actual transaction's status in your db
         $commande->set_transactionId($id_transaction);
@@ -29,12 +31,14 @@ if (isset($_POST['cpm_trans_id'])) {
         }
         // get correct values for this transactions
         $CinetPay->setTransId($id_transaction)->getPayStatus();
+
         $payment_date = $CinetPay->chk_payment_date;
         $phone_number = $CinetPay->chk_phone_number;
         $phone_prefix = $CinetPay->chk_phone_prefix;
         $message = $CinetPay->chk_message;
         $code = $CinetPay->chk_code;
         $api_response_id = $CinetPay->chk_api_response_id;
+
         // set news values in the class
         $commande->set_prefix($phone_prefix);
         $commande->set_number($phone_number);
@@ -47,6 +51,7 @@ if (isset($_POST['cpm_trans_id'])) {
             // correct, we continue
              $commande->set_methode($CinetPay->chk_payment_method);
              $commande->set_operator_id($CinetPay->chk_operator_id);
+            
         } else {
             // transaction is not valid
         } 
@@ -55,11 +60,7 @@ if (isset($_POST['cpm_trans_id'])) {
         echo "1"; // a retirer
     } catch (Exception $e) {
         echo "Erreur :" . $e->getMessage();
-        echo PHP_EOL;
-        echo json_encode($e);
     }
 } else {
-    echo "trop pas bon";
     // direct acces on IPN
 }
-?>
