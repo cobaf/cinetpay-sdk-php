d31b36bb3561aab4198871ed8ea8a7ed29207d3d
JeremieColle <jeremie.colle@cinetpay.com>
2021-08-04 14:36:28 +0000

    test ip

--- action.php
+++ action.php
@@ -8,6 +8,45 @@ require_once __DIR__ . '/src/new-guichet.php';
 include('commande.php');
 
 // This class manage "Commande" table in DB
+function ip_visitor_country()
+{
+
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $country  = "Unknown";
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://www.geoplugin.net/json.gp?ip=".$ip);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $ip_data_in = curl_exec($ch); // string
+    curl_close($ch);
+
+    $ip_data = json_decode($ip_data_in,true);
+    $ip_data = str_replace('&quot;', '"', $ip_data); // for PHP 5.2 see stackoverflow.com/questions/3110487/
+
+    if($ip_data && $ip_data['geoplugin_countryName'] != null) {
+        $country = $ip_data['geoplugin_countryName'];
+    }
+
+    return 'IP: '.$ip.' # Country: '.$country;
+}
+
+echo ip_visitor_country(); // output Coutry name
+die();
 $commande = new Commande();
 try {
     if(isset($_POST['valider']))
