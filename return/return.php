25e1d7d54771c0a937f460cb8d256bdfbb044cc8
JeremieColle <jeremie.colle@cinetpay.com>
2021-08-17 15:04:07 +0000

    Update return.php

--- return/return.php
+++ return/return.php
@@ -14,7 +14,7 @@ if (isset($_POST['payment_token'])) {
             die();
           }
           else {
-            header('Location: '.$commande->getCurrentUrl().'cinetpay-sdk-php/cinetpay-sdk-php');
+            header('Location: '.$commande->getCurrentUrl().'cinetpay-sdk-php/');
             echo 'Echec, votre paiement a échoué';
         }
  
@@ -22,6 +22,6 @@ if (isset($_POST['payment_token'])) {
         echo "Erreur :" . $e->getMessage();
     }
 } else {
-    header('Location: '.$commande->getCurrentUrl().'cinetpay-sdk-php/cinetpay-sdk-php');
+    header('Location: '.$commande->getCurrentUrl().'cinetpay-sdk-php/');
     die();
 }
